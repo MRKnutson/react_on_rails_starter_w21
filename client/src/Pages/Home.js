87269479787af309bd01127adf82dc353448b76b
Michael Knutson <mrknutson44@gmail.com>
2021-12-13 09:24:26 -0900

    added useToggle hook

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import useToggle from '../Hooks/useToggle';
 
 const Home = () => {
+  const { Toggler, showToggle } = useToggle(true)
+
+
   return (
     <div>
       <h1>Home</h1>
+      {showToggle && <p>Test Toggler</p>}
+      <button onClick = {Toggler}>Toggle</button>
     </div>
   );
 };
